225f67fd65cead205553e61376cc532d12f3163b
Emulond Argent <108662981+Emulond@users.noreply.github.com>
2024-06-30 22:40:00 +0300

    Added ru_RU to index.js
    Signed-off-by: Emulond Argent <108662981+Emulond@users.noreply.github.com>

--- webroot/js/lang/index.js
+++ webroot/js/lang/index.js
@@ -1,6 +1,7 @@
 import { en_US } from './en_US.js'
 import { pt_BR } from './pt_BR.js'
 import { ro_RO } from './ro_RO.js'
+import { ru_RU } from './ru_RU.js'
 import { vi_VN } from './vi_VN.js'
 
 export const lang = {
