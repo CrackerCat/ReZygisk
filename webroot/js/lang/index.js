158216c5e332b61b3f03fafd60ccb1690bad95cd
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-06-30 18:35:48 +0300

    add: `ro_RO` language to web UI (#17)
    This commit adds Romanian to the web UI language list.

--- webroot/js/lang/index.js
+++ webroot/js/lang/index.js
@@ -1,9 +1,11 @@
 import { en_US } from './en_US.js'
-import { vi_VN } from './vi_VN.js'
 import { pt_BR } from './pt_BR.js'
+import { ro_RO } from './ro_RO.js'
+import { vi_VN } from './vi_VN.js'
 
 export const lang = {
   en_US,
   pt_BR,
+  ro_RO,
   vi_VN,
 }
---
+++ webroot/js/lang/ro_RO.js
@@ -0,0 +1,42 @@
+/* INFO: Translated by @ExtremeXT */
+
+export const ro_RO = {
+  langName: 'Română (România)',
+  langModal: {
+    header: "Alegeți limba prestabilită"
+  },
+  infoCard: {
+    status: {
+      unknown: 'ReZygisk este într-o stare necunoscută!',
+      notWorking: 'ReZygisk nu este în funcțiune!',
+      ok: 'ReZygisk funcționează în totalitate!',
+      partially: 'ReZygisk funcționează parțial!'
+    },
+    version: 'Versiune',
+    root: 'Implementare Root',
+    zygote: {
+      injected: 'Injectat',
+      notInjected: 'Neinjectat',
+      unknown: 'Necunoscut'
+    }
+  },
+  moduleCard: {
+    header: 'Module',
+    arch: 'Arhitectură: '
+  },
+  settings: {
+    header: "Setări",
+    daemonButton: {
+      start: 'Start',
+      stop: 'Stop',
+      exit: 'Ieșire'
+    }
+  },
+  cmdErrors: {
+    ptrace64: 'Eroare în timpul executării zygisk-ptrace64',
+    ptrace32: 'Eroare în timpul executării zygisk-ptrace32',
+    ls: 'Eroare în timpul listării fișierelor din folderul Zygisk al modulului',
+    cat: 'Eroare în timpul citirii fișierului module.prop din modul',
+    find: 'Eroare la găsirea modulelor Zygisk'
+  },
+}
