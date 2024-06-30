a9fc2f6836069a5baf9b20c4c8077365ab111ca4
Emulond Argent <108662981+Emulond@users.noreply.github.com>
2024-06-30 22:55:10 +0300

    add: `ru_RU` language to web UI (#18)
    This commit adds Russian to the web UI language list.
    
    Signed-off-by: Emulond Argent <108662981+Emulond@users.noreply.github.com>

--- webroot/js/lang/index.js
+++ webroot/js/lang/index.js
@@ -1,11 +1,13 @@
 import { en_US } from './en_US.js'
 import { pt_BR } from './pt_BR.js'
 import { ro_RO } from './ro_RO.js'
+import { ru_RU } from './ru_RU.js'
 import { vi_VN } from './vi_VN.js'
 
 export const lang = {
   en_US,
   pt_BR,
   ro_RO,
+  ru_RU,
   vi_VN,
 }
