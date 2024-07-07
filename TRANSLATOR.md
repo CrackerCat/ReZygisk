90232b9eed75f62c209401c1a68467d7e17cefbd
Fyphen <fyphensub@gmail.com>
2024-07-07 11:51:23 +0700

    add: japanese translation

--- TRANSLATOR.md
+++ TRANSLATOR.md
@@ -1,5 +1,6 @@
 # 🌎 Translator
 - **en_US** by [@PerformanC (The PerformanC Organization)](https://github.com/PerformanC)
+- **ja_JP** by [@Fyphen1223](https://github.com/Fyphen1223)
 - **pt_BR** by [@ThePedroo](https://github.com/ThePedroo)
 - **ro_RO** by [@ExtremeXT](https://github.com/ExtremeXT)
 - **ru_RU** by [@Emulond](https://github.com/Emulond)
--- webroot/js/language.js
+++ webroot/js/language.js
@@ -130,8 +130,10 @@ export async function getTranslations(locate) {
   return await translateData.json()
 }
 
+/* INFO: This list is in alphabetical order. */
 export const avaliableLanguages = [
   "en_US", /* INFO: Translated by @PerformanC (The PerformanC Organization) */
+  "ja_JP", /* INFO: Translated by @Fyphen1223 */
   "pt_BR", /* INFO: Translated by @ThePedroo */
   "ro_RO", /* INFO: Translated by @ExtremeXT */
   "ru_RU", /* INFO: Translated by Emulond Argent (@Emulond) */
